1afac29e8f4b6430c0e15f6b0cb925824bcf1749
YShan98 <tanyingshan4850@gmail.com>
2020-12-23 21:45:04 +0800

    Update performance.vue

--- frontend-vue/src/pages/performance.vue
+++ frontend-vue/src/pages/performance.vue
@@ -7,11 +7,11 @@
       >
         <stats-card data-background-color="green">
           <template slot="header">
-            <md-icon>store</md-icon>
+            <md-icon>text_snippet</md-icon>
           </template>
 
           <template slot="content">
-            <p class="category">Revenue</p>
+            <p class="category">Total Number of Purchase Order in 2020</p>
             <h3 class="title">$34,245</h3>
           </template>
 
@@ -205,6 +205,8 @@
 </template>
 
 <script>
+import performance from "@/js/performance.js";
+
 import {
   StatsCard,
   ChartCard,
@@ -223,91 +225,20 @@ export default {
   },
   data() {
     return {
-      dailySalesChart: {
-        data: {
-          labels: ["M", "T", "W", "T", "F", "S", "S"],
-          series: [[12, 17, 7, 17, 23, 18, 38]]
-        },
-        options: {
-          lineSmooth: this.$Chartist.Interpolation.cardinal({
-            tension: 0
-          }),
-          low: 0,
-          high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-          chartPadding: {
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0
-          }
-        }
-      },
-      dataCompletedTasksChart: {
-        data: {
-          labels: ["12am", "3pm", "6pm", "9pm", "12pm", "3am", "6am", "9am"],
-          series: [[230, 750, 450, 300, 280, 240, 200, 190]]
-        },
-
-        options: {
-          lineSmooth: this.$Chartist.Interpolation.cardinal({
-            tension: 0
-          }),
-          low: 0,
-          high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-          chartPadding: {
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0
-          }
-        }
-      },
-      emailsSubscriptionChart: {
-        data: {
-          labels: [
-            "Ja",
-            "Fe",
-            "Ma",
-            "Ap",
-            "Mai",
-            "Ju",
-            "Jul",
-            "Au",
-            "Se",
-            "Oc",
-            "No",
-            "De"
-          ],
-          series: [[542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]]
-        },
-        options: {
-          axisX: {
-            showGrid: false
-          },
-          low: 0,
-          high: 1000,
-          chartPadding: {
-            top: 0,
-            right: 5,
-            bottom: 0,
-            left: 0
-          }
-        },
-        responsiveOptions: [
-          [
-            "screen and (max-width: 640px)",
-            {
-              seriesBarDistance: 5,
-              axisX: {
-                labelInterpolationFnc: function(value) {
-                  return value[0];
-                }
-              }
-            }
-          ]
-        ]
-      }
+      isEmpty: false,
+      performanceData: [],
     };
+  },
+  async created() {
+    try {
+      this.isLoading = true;
+      const data = await performance.get_performance(2020);
+      this.performanceData = data;
+      this.isLoading = false;
+    } catch (err) {
+      this.isLoading = false;
+      this.error = err.message;
+    }
   }
 };
 </script>
