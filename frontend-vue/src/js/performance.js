f6d74b4a41947d4b8d45019a0c594de25d947fa2
nivleM-ed <melvinedward98@gmail.com>
2021-01-15 20:24:49 +0800

    Change request type for getting performance

--- frontend-vue/src/js/performance.js
+++ frontend-vue/src/js/performance.js
@@ -7,7 +7,7 @@ class performance {
   static get_performance(year) {
     return new Promise(async (resolve, reject) => {
       try {
-        const res = await axios.get(`${url}get_overall`, {
+        const res = await axios.post(`${url}get_overall`, {
             year
         },{
           withCredentials: true
