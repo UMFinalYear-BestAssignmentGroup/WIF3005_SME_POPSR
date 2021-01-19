d5cfa34ab35ea68315a16e3facfbac83e3b64d64
nivleM-ed <melvinedward98@gmail.com>
2021-01-19 10:19:44 +0800

    Change request type to post

--- backend-node/routes/performance.js
+++ backend-node/routes/performance.js
@@ -5,6 +5,6 @@ var perf = require('../controller/performance');
 let {isLoggedIn, auth_no_t1, auth_no_t1_t2} = require('../middleware/authenticate');
 
 router.post('/get_overall', isLoggedIn, perf.get_performance_overall);
-router.get('/all', perf.get_all_user_performance);
+router.post('/all', perf.get_all_user_performance);
 
 module.exports = router;
